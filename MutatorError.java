4da248414f3199eb8c1575d863002746a8c0fc85
AryavShastri <shastri5219@mydusd.org>
2026-01-11 18:45:27 -0800

    Add MutatorError and Player classes

---
+++ MutatorError.java
@@ -0,0 +1,13 @@
+/*
+ * Activity 2.2.4
+ */
+public class MutatorExample2
+{
+  public static void main(String[] args) 
+  {
+    Player p = new Player(); 
+    p.setName();
+    System.out.println("Welcome " + p.getName());
+
+  }
+}
